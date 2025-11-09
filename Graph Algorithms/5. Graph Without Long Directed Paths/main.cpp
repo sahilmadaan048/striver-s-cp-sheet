2fa9d51410a2b56f3b27d62c7ff7afe51a1a7de4
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-09 19:08:13 +0530

    5. Graph without using directed graphs - check bipartite

---
+++ Graph Algorithms/5. Graph Without Long Directed Paths/main.cpp
@@ -0,0 +1,140 @@
+// https://codeforces.com/contest/1144/problem/F
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
+using namespace std;
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
+// ================================== take ip/op like vector,pairs directly!==================================
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
+// ===================================END Of the input module ==========================================
+
+// check bipartite grah
+bool dfs(int node, int color, unordered_map<int, vector<int>> &mp, vector<int> &ans)
+{
+    ans[node] = color;
+
+    for (auto adj : mp[node])
+    {
+        if (ans[adj] == -1)
+        {
+            if (!dfs(adj, 1 - color, mp, ans))
+                return false;
+        }
+        else if (ans[adj] == color)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    unordered_map<int, vector<int>> mp;
+    vector<pair<int, int>> edges;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        mp[u].push_back(v);
+        mp[v].push_back(u);
+        edges.push_back({u, v});
+    }
+    vector<int> ans(n + 1, -1);
+    bool ok = true;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (ans[i] == -1)
+        {
+            if (!dfs(i, 0, mp, ans))
+            {
+                ok = false;
+                break;
+            }
+        }
+    }
+
+    if (!ok)
+    {
+        cout << "NO\n";
+        return;
+    }
+
+    cout << "YES\n";
+    for (auto [u, v] : edges)
+    {
+        if (ans[u] == 0 && ans[v] == 1)
+            cout << 1;
+        else
+            cout << 0;
+    }
+    cout << "\n";
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
