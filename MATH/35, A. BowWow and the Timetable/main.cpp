cedd7d92f5f1f21550f85d408dd7ed8c18929f30
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-12 22:58:41 +0530

    MATH - A. BowWow and the timetable

--- MATH/35, A. BowWow and the Timetable/main.cpp
+++ MATH/35, A. BowWow and the Timetable/main.cpp
@@ -1,59 +1,91 @@
-// https://codeforces.com/problemset/problem/1204/A 
+// https://codeforces.com/problemset/problem/1204/A
 
 #include "bits/stdc++.h"
 #define int long long
 #define uint unsigned long long
 #define vi vector<int>
-#define vvi vector<vi >
+#define vvi vector<vi>
 #define vb vector<bool>
-#define vvb vector<vb >
-#define fr(i,n) for(int i=0; i<(n); i++)
-#define rep(i,a,n) for(int i=(a); i<=(n); i++)
-#define nl cout<<"\n"
-#define dbg(var) cout<<#var<<"="<<var<<" "
-#define all(v) v.begin(),v.end()
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
 #define sz(v) (int)(v.size())
-#define srt(v)  sort(v.begin(),v.end())         // sort 
-#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
-#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
-#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-// make sure to sort before applying unique // else only consecutive duplicates would be removed 
-#define bin(x,y)  bitset<y>(x) 
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
 using namespace std;
-int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
 
-
-
-void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
-void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
-void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
 // ================================== take ip/op like vector,pairs directly!==================================
-template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
-template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
-template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
 // ===================================END Of the input module ==========================================
 
+void solve()
+{
+    string s;
+    cin >> s;
 
-void solve(){
-    string s; cin >> s;     
     int l = s.size();
-    string temp(l , '0');
-    temp[0] = '1';
     int ans = (l + 1) / 2;
-    if(l % 2 and s == temp) {
-        ans--;
+
+    bool isPowerOf4 = (s[0] == '1');
+    for (int i = 1; i < l; i++)
+    {
+        if (s[i] != '0')
+        {
+            isPowerOf4 = false;
+            break;
+        }
     }
+
+    // if it is power of 4 and len in odd, reduce 1
+    if (isPowerOf4 && (l % 2 == 1))
+        ans--;
     cout << ans << "\n";
-    return;
 }
 
 int32_t main()
 {
- 
- ios_base::sync_with_stdio(false);
- cin.tie(NULL);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int T = 1;
     while (T--)
@@ -62,5 +94,3 @@ int32_t main()
     }
     return 0;
 }
-
-    
