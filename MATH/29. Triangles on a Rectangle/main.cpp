0810d05195312b9a25968812143eaca7abc76fa4
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-12 20:30:33 +0530

    29. Triangles on a rectangle

--- MATH/29. Triangles on a Rectangle/main.cpp
+++ MATH/29. Triangles on a Rectangle/main.cpp
@@ -4,47 +4,87 @@
 #define int long long
 #define uint unsigned long long
 #define vi vector<int>
-#define vvi vector<vi >
+#define vvi vector<vi>
 #define vb vector<bool>
-#define vvb vector<vb >
-#define fr(i,n) for(int i=0; i<(n); i++)
-#define rep(i,a,n) for(int i=(a); i<=(n); i++)
-#define nl cout<<"\n"
-#define dbg(var) cout<<#var<<"="<<var<<" "
-#define all(v) v.begin(),v.end()
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
 #define sz(v) (int)(v.size())
-#define srt(v)  sort(v.begin(),v.end())         // sort 
-#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
-#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
-#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-// make sure to sort before applying unique // else only consecutive duplicates would be removed 
-#define bin(x,y)  bitset<y>(x) 
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
 using namespace std;
-int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
 
-
-
-void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
-void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
-void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
 // ================================== take ip/op like vector,pairs directly!==================================
-template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
-template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
-template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
 // ===================================END Of the input module ==========================================
 
+void solve()
+{
+    int w, h;
+    cin >> w >> h;
+    int ans = 0;
 
-void solve(){
-    int w, h; cin >> w >> h;
-} 
+    for (int i = 0; i < 4; i++)
+    {
+        int k;
+        cin >> k; // number of points
+        vector<int> a(k);
+        for (int j = 0; j < k; j++)
+            cin >> a[j];
+        int diff = a.back() - a.front();
+        if (i < 2)
+            ans = max(ans, diff * h);
+        else
+            ans = max(ans, diff * w);
+    }
+
+    cout << ans << "\n";
+}
 
 int32_t main()
 {
- 
- ios_base::sync_with_stdio(false);
- cin.tie(NULL);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int T = 1;
     cin >> T;
@@ -54,5 +94,3 @@ int32_t main()
     }
     return 0;
 }
-
-    
