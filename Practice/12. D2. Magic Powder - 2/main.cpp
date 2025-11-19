08a8d29117e2ab46d60526d445baa5a5d07defca
Sahil Madaan <madaan.sahil27@gmail.com>
2025-11-19 22:22:43 +0530

    Practice - D2. Magic Powder - 2

---
+++ Practice/12. D2. Magic Powder - 2/main.cpp
@@ -0,0 +1,110 @@
+// https://codeforces.com/contest/670/problem/D2
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
+using namespace std;
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
+// ================================== take ip/op like vector,pairs directly!==================================
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
+// ===================================END Of the input module ==========================================
+
+bool check(int d, int n, vector<int> &a, vector<int> &b, int k)
+{
+    long long need = 0;
+    for (int i = 0; i < n; i++)
+    {
+        need += max(0LL, a[i] * d - b[i]);
+        if (need > k)
+            return false;
+    }
+    return true;
+}
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n), b(n);
+    for (int &x : a)
+        cin >> x;
+    for (int &x : b)
+        cin >> x;
+
+    long long lo = 0, hi = 2e9;
+
+    while (lo <= hi)
+    {
+        long long mid = lo + (hi - lo) / 2;
+
+        if (check(mid, n, a, b, k))
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+    }
+
+    cout << hi << "\n";
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T = 1;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
